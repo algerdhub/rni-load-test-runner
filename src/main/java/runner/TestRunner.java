4fe377f6fb786b1f89edf3459b20ce0dae7b24ef
Alexander <algerd-mail@mail.ru>
2014-04-25 17:06:05 +0300

    added proerties file

--- src/main/java/runner/TestRunner.java
+++ src/main/java/runner/TestRunner.java
@@ -1,7 +1,18 @@
 package main.java.runner;
 
+import main.java.runner.utils.PropertyLoader;
+
+import java.io.FileNotFoundException;
+
 public class TestRunner {
     public static void main(String[] args){
         System.out.println("dsf");
+        PropertyLoader propertyLoader = new PropertyLoader("application.properties");
+        try {
+            System.out.println(propertyLoader.loadProperty("test"));
+            System.out.println(propertyLoader.loadProperty("notest"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/runner/utils/PropertyLoader.java
+++ src/main/java/runner/utils/PropertyLoader.java
@@ -1,21 +1,25 @@
 package main.java.runner.utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyLoader {
 
-    private final String prop_file;
+    private final File prop_file;
 
     public PropertyLoader(String filename)
     {
-        this.prop_file = filename;
+        this.prop_file = new File(filename);
     }
 
-    public String loadProperty(String propertyName){
+    public String loadProperty(String propertyName) throws FileNotFoundException {
         Properties props = new Properties();
+        FileReader reader = new FileReader(prop_file);
         try {
-            props.load(PropertyLoader.class.getResourceAsStream(prop_file));
+            props.load(reader);
         } catch (IOException e) {
             e.printStackTrace();
         }
